6999e6f81ea149b3b9589b3ce049a01a2f86a2ab
ibywater <isabella.bywater@outlook.com>
2026-05-03 18:37:01 +1000

    Update and rename samtools.sh to 4.sort.sh

--- 4.sort.sh
+++ 4.sort.sh
@@ -4,19 +4,15 @@
 
 
 # DFT1
+samtools sort DFT_TD_input_trimmed.bam  -o sorted/DFT_TD_input_sorted.bam
+samtools sort DFT_TD_H3K4me3_trimmed.bam -o sorted/DFT_TD_H3K4me3_sorted.bam
 samtools sort DFT_TD_H3K9ac_trimmed.bam -o sorted/DFT_TD_H3K9ac_sorted.bam
 samtools sort DFT_TD_H3K9me3_trimmed.bam -o sorted/DFT_TD_H3K9me3_sorted.bam
 samtools sort DFT_TD_H4K20me3_trimmed.bam -o sorted/DFT_TD_H4K20me3_sorted.bam
 
 # Fibroblasts
+samtools sort F_TD_input_trimmed.bam -o sorted/F_TD_input_sorted.b
+samtools sort F_TD_H3K4me3_trimmed.bam -o sorted/F_TD_H3K4me3_sorted.bam
 samtools sort F_TD_H3K9ac_trimmed.bam -o sorted/F_TD_H3K9ac_sorted.bam
 samtools sort F_TD_H3K9me3_trimmed.bam -o sorted/F_TD_H3K9me3_sorted.bam
 samtools sort F_TD_H4K20me3_trimmed.bam -o sorted/F_TD_H4K20me3_sorted.bam
-
-#inputs
-samtools sort F_TD_input_trimmed.bam -o sorted/F_TD_input_sorted.bam
-samtools sort DFT_TD_input_trimmed.bam  -o sorted/DFT_TD_input_sorted.bam
-
-#Pos control H3K4me3
-samtools sort F_TD_H3K4me3_trimmed.bam -o sorted/F_TD_H3K4me3_sorted.bam
-samtools sort DFT_TD_H3K4me3_trimmed.bam -o sorted/DFT_TD_H3K4me3_sorted.bam
