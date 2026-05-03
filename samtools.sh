0cc8aedafd9cbf720a6b09ff6341b1413e123999
ibywater <isabella.bywater@outlook.com>
2026-05-03 18:30:50 +1000

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -20,6 +20,3 @@ samtools sort DFT_TD_input_trimmed.bam  -o sorted/DFT_TD_input_sorted.bam
 #Pos control H3K4me3
 samtools sort F_TD_H3K4me3_trimmed.bam -o sorted/F_TD_H3K4me3_sorted.bam
 samtools sort DFT_TD_H3K4me3_trimmed.bam -o sorted/DFT_TD_H3K4me3_sorted.bam
-
-# jess unedited
-samtools sort /srv/scratch/waters/Jess/Subread/M_TD_H3K9me3_trimmed.bam -o /srv/scratch/waters/Jess/Subread/sorted/M_TD_H3K9me3_sorted.bam
